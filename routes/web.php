792478bf9c47bfa94db718641bcaae481a316ec6
kadankyi1 <annodankyikwaku@gmail.com>
2022-12-30 22:53:39 -0800

    Added adminer route to web routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// ADMINER DATABASE MANAGEMENT TOOL
+Route::any('adminer', '\Aranyasen\LaravelAdminer\AdminerAutologinController@index');
+

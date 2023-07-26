5feb2e3e2ffec7b59a50a90cebfc18b510d2030a
kadankyi1 <annodankyikwaku@gmail.com>
2023-07-26 05:49:15 -0700

    Updated app section wording on list, search, reader and privacy policy pages

--- resources/views/webapp/reader.blade.php
+++ resources/views/webapp/reader.blade.php
@@ -209,10 +209,15 @@ if(!empty($_GET["trxref"]) && !empty($_GET["reference"])){
 <div class="middle py-5">
   <div class="container py-xl-5 py-lg-3">
     <div class="welcome-left text-center py-md-3 mb-md-5">
-      <h3 class="mb-4">Search & Read on our mobile Apps</h3>
-      <p class="text-italic">Find a summary on the mobile app, use the reference number to pay on our website and read on your mobile apps or on our website. Click the button below to download your desired mobile app</p>
-      <a href="https://play.google.com/store/apps/details?id=com.tafarri.tafarri" target="_blank" class="btn btn-primary btn-style mt-md-5 mt-4 mr-2">Android App</a>
-      <a href="https://apps.apple.com/us/app/id1670395865" target="_blank" class="btn btn-white btn-style mt-md-5 mt-4">iPhone App</a>
+        <h3 class="mb-4">Our Sentiment</h3>
+        <p class="text-italic">We hope you enjoy and learn from our summaries half as much as we enjoy putting them together for you.</p>
+				
+        <!--
+        <h3 class="mb-4">Search & Read On Our Mobile Apps</h3>
+        <p class="text-italic">Find a summary on the mobile app, use the reference number to pay on our website and read on your mobile apps or on our website. Click the button below to download your desired mobile app</p>
+				<a href="https://play.google.com/store/apps/details?id=com.tafarri.tafarri" target="_blank" class="btn btn-primary btn-style mt-md-5 mt-4 mr-2">Android App</a>
+				<a href="https://apps.apple.com/us/app/id1670395865" target="_blank" class="btn btn-white btn-style mt-md-5 mt-4">iPhone App</a>
+        -->
     </div>
   </div>
 </div>

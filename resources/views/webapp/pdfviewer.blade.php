2f12fe3974652832f9fc29d974306d6098a2297a
kadankyi1 <annodankyikwaku@gmail.com>
2023-09-09 04:07:16 -0700

    Testing restricted pdf files viewer

--- resources/views/webapp/pdfviewer.blade.php
+++ resources/views/webapp/pdfviewer.blade.php
@@ -110,9 +110,9 @@ if(!empty($reader_book_url) && !empty($reader_book_file_name)){
     //header($header_disposition);
     
     header("Content-type: application/pdf");
-    header("Content-Disposition: inline; filename=" . $reader_book_file_name);
-    header('Content-Transfer-Encoding: binary');
-    header('Accept-Ranges: bytes');
+    //header("Content-Disposition: inline; filename=" . $reader_book_file_name);
+    //header('Content-Transfer-Encoding: binary');
+    //header('Accept-Ranges: bytes');
 
     @readfile($reader_book_url);
 

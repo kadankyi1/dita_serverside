9b54cc06b893c7f396dac0ac115d981197c48a61
kadankyi1 <annodankyikwaku@gmail.com>
2023-09-09 03:39:29 -0700

    Testing restricted pdf files viewer

--- resources/views/webapp/pdfviewer.blade.php
+++ resources/views/webapp/pdfviewer.blade.php
@@ -106,17 +106,17 @@ if(!empty($error)){
     //echo "error: " . $error;
 }
 if(!empty($reader_book_url) && !empty($reader_book_file_name)){
-    $header_disposition = "Content-Disposition: attachment; filename=" . $reader_book_file_name;
+    //$header_disposition = "Content-Disposition: attachment; filename=" . $reader_book_file_name;
 
-    
-    header("Content-type: application/pdf");
-    //header($header_disposition);
-    header("Content-Disposition: inline; filename=" . $reader_book_file_name);
-    @readfile($reader_book_url);
 
-    //echo "<br><br><br>reader_book_url: " . $reader_book_url;
-    //echo "<br><br><br>reader_book_file_name: " . $reader_book_file_name;
-    //echo "<br><br><br>header_disposition: " . $header_disposition;
+    //header("Content-type: application/pdf");
+    //---header($header_disposition);
+    //header("Content-Disposition: inline; filename=" . $reader_book_file_name);
+    //@readfile($reader_book_url);
+
+    echo "<br><br><br>reader_book_url: " . $reader_book_url;
+    echo "<br><br><br>reader_book_file_name: " . $reader_book_file_name;
+    echo "<br><br><br>header_disposition: " . $header_disposition;
 }
 
 ?>

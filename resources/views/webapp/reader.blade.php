a1bd01b7ba9ed98c7ad534a7cda61ae28c64714d
kadankyi1 <annodankyikwaku@gmail.com>
2023-09-12 04:19:00 -0700

    Using route auth for testing restricted PDF access

--- resources/views/webapp/reader.blade.php
+++ resources/views/webapp/reader.blade.php
@@ -17,8 +17,8 @@ if(!empty($_GET["trxref"]) && !empty($_GET["reference"])){
             ->take(1)
             ->get();
 
-    //var_dump($this_transaction[0]); exit;
-    if(empty($this_transaction) || empty($this_transaction[0]->transaction_sys_id)){
+    var_dump($this_transaction); exit;
+    if(empty($this_transaction) || empty($this_transaction[0]) || empty($this_transaction[0]->transaction_sys_id)){
         //echo "here 2"; exit;
         $error = "We could not verify your payment";
     } 
